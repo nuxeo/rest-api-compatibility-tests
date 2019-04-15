c0f61308f1d4cc6bdd2374def1aeb889be0c980a
Antoine Taillefer <ataillefer@nuxeo.com>
2019-04-15 12:35:47 +0200

    NXP-27102: add pagination parameters test

--- test/v1/user/expected/index.js
+++ test/v1/user/expected/index.js
@@ -5,6 +5,8 @@ const JACK_UPDATED = require('./jack-updated.json');
 const JOE = require('./joe.json');
 const JOE_USERPROFILE = require('./joe-userprofile.json');
 const SEARCH_ALL_USERS = require('./search-all-users.json');
+const SEARCH_ALL_USERS_PAGE_0 = require('./search-all-users-page-0.json');
+const SEARCH_ALL_USERS_PAGE_2 = require('./search-all-users-page-2.json');
 const SEARCH_JOE_USER = require('./search-joe-user.json');
 const SEARCH_NO_USER = require('./search-no-user.json');
 
@@ -16,6 +18,8 @@ module.exports = {
   JOE,
   JOE_USERPROFILE,
   SEARCH_ALL_USERS,
+  SEARCH_ALL_USERS_PAGE_0,
+  SEARCH_ALL_USERS_PAGE_2,
   SEARCH_JOE_USER,
   SEARCH_NO_USER,
 };
