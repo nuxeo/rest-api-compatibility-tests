3b8ec51623d3ce1e674a4ff25d1a675c9abc3454
Antoine Taillefer <ataillefer@nuxeo.com>
2019-04-15 16:20:41 +0200

    NXP-27170: add group endpoint fetch properties tests

--- test/v1/group/expected/index.js
+++ test/v1/group/expected/index.js
@@ -1,11 +1,13 @@
 const GROUP1 = require('./group1.json');
 const GROUP2 = require('./group2.json');
+const GROUP2_FETCH_PROPERTIES = require('./group2-fetch-properties.json');
 const GROUP2_UPDATED = require('./group2-updated.json');
 const JOE_GROUP2 = require('./joe-group2.json');
 
 module.exports = {
   GROUP1,
   GROUP2,
+  GROUP2_FETCH_PROPERTIES,
   GROUP2_UPDATED,
   JOE_GROUP2,
 };
