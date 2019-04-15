323d66a8732892a13140fe9d8383a6486c2e8bf0
Antoine Taillefer <ataillefer@nuxeo.com>
2019-04-15 12:35:47 +0200

    NXP-27102: add pagination parameters test

--- test/v1/user/user-search.js
+++ test/v1/user/user-search.js
@@ -2,6 +2,8 @@ const request = require('../request');
 
 const {
   SEARCH_ALL_USERS,
+  SEARCH_ALL_USERS_PAGE_0,
+  SEARCH_ALL_USERS_PAGE_2,
   SEARCH_JOE_USER,
   SEARCH_NO_USER,
 } = require('./expected');
@@ -31,6 +33,36 @@ t(
     });
     expect(statusCode).toBe(200);
     expect(body).toMatchObject(SEARCH_ALL_USERS);
+  },
+);
+
+t(
+  'GET /user/search > search for users with query returning all users and pagination parameters: pageSize=1, currentPageIndex=0',
+  async () => {
+    const { statusCode, body } = await request.get('/user/search', {
+      qs: {
+        q: '*',
+        pageSize: 1,
+        currentPageIndex: 0,
+      },
+    });
+    expect(statusCode).toBe(200);
+    expect(body).toMatchObject(SEARCH_ALL_USERS_PAGE_0);
+  },
+);
+
+t(
+  'GET /user/search > search for users with query returning all users and pagination parameters: pageSize=1, currentPageIndex=2',
+  async () => {
+    const { statusCode, body } = await request.get('/user/search', {
+      qs: {
+        q: '*',
+        pageSize: 1,
+        currentPageIndex: 2,
+      },
+    });
+    expect(statusCode).toBe(200);
+    expect(body).toMatchObject(SEARCH_ALL_USERS_PAGE_2);
   },
 );
 
