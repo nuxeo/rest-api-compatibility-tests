560ab9438462382bcc91ebca3493fd08c28f5b92
Antoine Taillefer <ataillefer@nuxeo.com>
2019-05-10 10:53:09 +0200

    NXP-27179: add group search tests, including pagination

--- test/v1/group/expected/index.js
+++ test/v1/group/expected/index.js
@@ -3,6 +3,11 @@ const GROUP2 = require('./group2.json');
 const GROUP2_FETCH_PROPERTIES = require('./group2-fetch-properties.json');
 const GROUP2_UPDATED = require('./group2-updated.json');
 const JOE_GROUP2 = require('./joe-group2.json');
+const SEARCH_ALL_GROUPS = require('./search-all-groups.json');
+const SEARCH_ALL_GROUPS_PAGE_0 = require('./search-all-groups-page-0.json');
+const SEARCH_ALL_GROUPS_PAGE_3 = require('./search-all-groups-page-3.json');
+const SEARCH_GROUP1_GROUP = require('./search-group1-group.json');
+const SEARCH_NO_GROUP = require('./search-no-group.json');
 
 module.exports = {
   GROUP1,
@@ -10,4 +15,9 @@ module.exports = {
   GROUP2_FETCH_PROPERTIES,
   GROUP2_UPDATED,
   JOE_GROUP2,
+  SEARCH_ALL_GROUPS,
+  SEARCH_ALL_GROUPS_PAGE_0,
+  SEARCH_ALL_GROUPS_PAGE_3,
+  SEARCH_GROUP1_GROUP,
+  SEARCH_NO_GROUP,
 };
