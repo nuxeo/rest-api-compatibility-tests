88d73fbc4a34d7867dc9f910137a9e88ca0d9205
Antoine Taillefer <ataillefer@nuxeo.com>
2019-05-17 11:09:47 +0200

    NXP-27391: fix user enricher test (user workspace root init issue)

--- test/common/personal-workspace.js
+++ test/common/personal-workspace.js
@@ -1,6 +1,17 @@
 const request = require('../request')('v1'); // force REST API v1
 
-const USER_WORKSPACES_PATH = '/default-domain/UserWorkspaces';
+const DEFAULT_DOMAIN_PATH = '/default-domain';
+const USER_WORKSPACES_PATH = `${DEFAULT_DOMAIN_PATH}/UserWorkspaces`;
+
+const createUserWorkspaceRoot = async () => {
+  await request.strictPost(`path${DEFAULT_DOMAIN_PATH}`, {
+    body: {
+      'entity-type': 'document',
+      name: 'UserWorkspaces',
+      type: 'UserWorkspacesRoot',
+    },
+  });
+};
 
 const createPersonalWorkspace = async (username) => {
   const { body } = await request.strictPost(`/path${USER_WORKSPACES_PATH}`, {
@@ -17,7 +28,13 @@ const deletePersonalWorkspace = async (username) => {
   await request.strictDel(`/path${USER_WORKSPACES_PATH}/${username}`);
 };
 
+const deleteUserWorkspaceRoot = async () => {
+  await request.strictDel(`/path${USER_WORKSPACES_PATH}`);
+};
+
 module.exports = {
   createPersonalWorkspace,
+  createUserWorkspaceRoot,
   deletePersonalWorkspace,
+  deleteUserWorkspaceRoot,
 };
