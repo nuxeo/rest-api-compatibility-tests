38277eaccc8d133ae91c10bcfc9f56ba0165fca0
Thomas Roger <thomas.roger@hyland.com>
2026-04-24 11:18:22 +0200

    NXP-33560: migrate to modern dev stack
    - Node 24, npm (replacing Yarn Classic)
    - Vitest (replacing Jest 24)
    - Native fetch (replacing deprecated request-promise-native)
    - ESLint 9 flat config (replacing ESLint 5)
    - Prettier 3 (replacing Prettier 1)

--- test/request.js
+++ test/request.js
@@ -1,5 +1,3 @@
-const request = require('request-promise-native');
-
 const NUXEO_SERVER_URL =
   process.env.NUXEO_SERVER_URL || 'http://localhost:8080/nuxeo';
 const NUXEO_API_URL = `${NUXEO_SERVER_URL}/api`;
@@ -11,8 +9,7 @@ const ADMIN_AUTH = {
 
 const DEFAULT_OPTIONS = {
   auth: ADMIN_AUTH,
-  simple: false, // don't fail with StatusCodeError in case of 404
-  resolveWithFullResponse: true,
+  simple: false, // don't fail with an error in case of non-2xx status code
   json: true,
 };
 
@@ -55,14 +52,65 @@ const DEFAULT_OPTIONS = {
  */
 module.exports = (version) => {
   const r = async (path, options) => {
-    const computedURI = `${NUXEO_API_URL}/${version}/${path}`;
-    const uri = computedURI.replace(/([^:])\/\/+/, '$1/');
-    const requestOptions = {
-      ...DEFAULT_OPTIONS,
-      ...options,
-      uri,
+    const mergedOptions = { ...DEFAULT_OPTIONS, ...options };
+    const {
+      auth,
+      simple,
+      json,
+      body,
+      method,
+      qs,
+      headers: extraHeaders,
+    } = mergedOptions;
+
+    const computedURL = `${NUXEO_API_URL}/${version}/${path}`;
+    const url = new URL(computedURL.replace(/([^:])\/\/+/, '$1/'));
+    if (qs) {
+      Object.entries(qs).forEach(([key, value]) => {
+        url.searchParams.set(key, value);
+      });
+    }
+
+    const headers = { ...extraHeaders };
+    if (auth) {
+      const encoded = Buffer.from(`${auth.user}:${auth.pass}`).toString(
+        'base64',
+      );
+      headers.Authorization = `Basic ${encoded}`;
+    }
+    if (json && body) {
+      headers['Content-Type'] = 'application/json';
+    }
+
+    const fetchOptions = {
+      method: method || 'GET',
+      headers,
     };
-    return request(requestOptions);
+    if (body) {
+      fetchOptions.body = json ? JSON.stringify(body) : body;
+    }
+
+    const response = await fetch(url, fetchOptions);
+
+    let responseBody;
+    const contentType = response.headers.get('content-type') || '';
+    const text = await response.text();
+    if (json && contentType.includes('application/json') && text) {
+      responseBody = JSON.parse(text);
+    } else {
+      responseBody = text;
+    }
+
+    if (simple && !response.ok) {
+      const error = new Error(
+        `${response.status} - ${response.statusText}: ${typeof responseBody === 'string' ? responseBody : JSON.stringify(responseBody)}`,
+      );
+      error.statusCode = response.status;
+      error.response = { body: responseBody };
+      throw error;
+    }
+
+    return { statusCode: response.status, body: responseBody };
   };
 
   r.get = async (path, options) => r(path, { ...options, method: 'GET' });
@@ -70,7 +118,7 @@ module.exports = (version) => {
   r.put = async (path, options) => r(path, { ...options, method: 'PUT' });
   r.del = async (path, options) => r(path, { ...options, method: 'DELETE' });
 
-  // fail with StatusCodeError in case of status code other than 2xx
+  // fail with an error in case of status code other than 2xx
   r.strictGet = async (path, options) =>
     r.get(path, { ...options, simple: true });
   r.strictPost = async (path, options) =>
